d8d393b3449c28442adcfbac792167c2525403fa
AshwinSuthar <AshwinSuthar@ASHWIN>
2023-08-14 00:27:36 +0530

    ashwin test multiple nb web java project in git together -ashwin140823

---
+++ WebApplication1/src/java/calculator/Demo.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+import java.util.*;
+/**
+ *
+ * @author AshwinSuthar
+ */
+public class Demo {
+    // Java program illustrating use of HashMap methods -Ashwin Added 230723
+
+	public static void main(String args[])
+	{
+		// Creation of HashMap
+		HashMap<String, String> Geeks = new HashMap<>();
+
+		// Adding values to HashMap using put() method
+		Geeks.put("Language", "Java");
+		Geeks.put("Platform", "Geeks For Geeks");
+		Geeks.put("Code", "HashMap");
+		Geeks.put("Learn", "More");
+
+		// Printing out the values of the HashMap
+		System.out.println("HashMap Contents: " + Geeks);
+		// Printing the size of the HashMap
+		System.out.println("The size of the HashMap is: "
+						+ Geeks.size());
+
+		// Using the get() method to retrieve a value from
+		// the HashMap
+		String language = Geeks.get("Language");
+		System.out.println("Language: " + language);
+
+		// Using the containsKey() method to check if a key
+		// exists in the HashMap
+		boolean containsKey = Geeks.containsKey("Code");
+		System.out.println("Contains Key 'Code'? "
+						+ containsKey);
+
+		// Using the containsValue() method to check if a
+		// value exists in the HashMap
+		boolean containsValue = Geeks.containsValue("More");
+		System.out.println("Contains Value 'More'? "
+						+ containsValue);
+
+		// Using the keySet() method to retrieve a Set of
+		// all keys in the HashMap
+		Set<String> keySet = Geeks.keySet();
+		System.out.println("Key Set: " + keySet);
+
+		// Using the values() method to retrieve a
+		// Collection of all values in the HashMap
+		Collection<String> values = Geeks.values();
+		System.out.println("Values: " + values);
+
+		// Using the entrySet() method to retrieve a Set of
+		// all key-value pairs in the HashMap
+		Set<Map.Entry<String, String> > entrySet
+			= Geeks.entrySet();
+		System.out.println("Entry Set: " + entrySet);
+
+		// Using the remove() method to remove a key-value
+		// pair from the HashMap
+		String removedValue = Geeks.remove("Code");
+		System.out.println("Removed Value: "
+						+ removedValue);
+
+		// Using the clear() method to remove all key-value
+		// pairs from the HashMap
+		Geeks.clear();
+		System.out.println(
+			"HashMap Contents after clear(): " + Geeks);
+
+		// Using the isEmpty() method to check if the
+		// HashMap is empty
+		boolean isEmpty = Geeks.isEmpty();
+		System.out.println("Is Empty? " + isEmpty);
+	}
+}
+
---
+++ WebApplication1/web/newTest.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : newTest
+    Modified on : Jul 24, 2023, 01:50:00 AM
+    Author     : rameelasuthar added 240723-1a31m for conflict & rebase
+
+    Author     : theashwins new Data added for conflict 240723-purposely before taking rameelasutharupdate 1a50m
+
+
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!--added by rameelasuthar for conflict testing theashwins for 240723-->
+<!--added by theashwins for conflict testing theashwins for 240723 at 1a50m-->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
